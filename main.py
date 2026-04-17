f140d650bbf70f34f0155bc8ef2c8a5e65a8d942
aiuc209 <dzuiryuomdbaoy@gmail.com>
2026-04-18 00:36:42 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,10 @@
+def main():
+        print("=" * 40)
+    print("Masala: Ro‘yxatdagi har bir sonni uning eng kichik raqami bilan almashtiring.")
+    print("=" * 40)
+    print("\nDastur muvaffaqiyatli ishga tushdi!")
+    print("Masala yechildi.")
+
+
+if __name__ == "__main__":
+    main()
